353ed3d37708ab233739a4fc8633192221200161
Mohammed Khan <abukhan363@gmail.com>
2024-07-14 20:19:55 -0400

    Init NextJS app, set up basic shadcn ui dashboard

---
+++ components/about-dialog.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { CircleHelp } from "lucide-react"
+import Link from "next/link"
+
+export function AboutDialog() {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="mt-auto rounded-lg"
+          aria-label="Help"
+          >
+              <CircleHelp className="size-5" />
+          </Button>
+      </DialogTrigger>
+      <DialogContent className="lg">
+        <DialogHeader>
+          <DialogTitle>About The Bear</DialogTitle>
+          <DialogDescription>
+              The Bear is a LLM-based platform to generate, bookmark, and share recipies.
+              A LLM nicknamed Bear will help you create recipies based on your preferences.
+               This app is named after the critically-acclaimed show <Link href="https://www.rottentomatoes.com/tv/the_bear" target="_blank" className="underline text-blue-600 hover:text-blue-800">The Bear</Link>.
+          </DialogDescription>
+        </DialogHeader>
+        {/* <ProfileForm /> */}
+      </DialogContent>
+    </Dialog>
+  )
+}
